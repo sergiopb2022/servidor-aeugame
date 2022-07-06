6cb34684523c6f53d0b9654cbb72bffacb936128
Sérgio Pires Barra <spbarra2022@gmail.com>
2022-07-05 23:32:14 -0300

    multiplayer idplayer idservidor rotacao

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ const server = app.listen(PORT, () => {
 const wss = new WebSocket.Server({ server })
 
 const clients = {}
+const partidas = {}
+const partidaID = uuid.v4(); //teste
+
 let qtdjogadoresingame = 0
 
 wss.on('connection', (ws)=>{
@@ -25,6 +28,25 @@ wss.on('connection', (ws)=>{
     clients[playerID]= {id:playerID}
     console.log("conectou jogador de ID: "+playerID)
     let size = Object.keys(clients).length;
+
+    wss.clients.forEach(function each(client) {
+      if (client == ws && client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify({
+          type: 'idplayer',
+          id: playerID,
+        }))
+      }
+    });
+
+    wss.clients.forEach(function each(client) {
+      if (client == ws && client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify({
+          type: 'partida',
+          partida: partidaID,
+        }))
+      }
+    });
+
    /*  if(qtdjogadoresingame >1){
         console.log("spawn players!!")
         wss.clients.forEach(function each(client) {
@@ -69,6 +91,7 @@ wss.on('connection', (ws)=>{
                             console.log("2 jogadores na fase1")
                             client.send(JSON.stringify({
                                 type: 'spawn-player',
+                                id: playerID,
                              }))
                             }
                         }
@@ -81,9 +104,13 @@ wss.on('connection', (ws)=>{
                         if (client !== ws && client.readyState === WebSocket.OPEN) {
                           client.send(JSON.stringify({
                             type: 'posicao-jogadores',
+                            idPlayer: playerID,
+                            idPartida: partidaID,
                             x: (packet.x).toString(),
                             y: (packet.y).toString(),
-                            z: (packet.z).toString()
+                            z: (packet.z).toString(),
+                            ry: (packet.ry).toString(),
+                            anim: (packet.anim).toString(),
                           }))
                         }
                       });
